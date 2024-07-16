9bec76e411da5e319c247d090b812459685eca75
D-Agbikossi <d.agbikossi@alustudent.com>
2024-07-16 05:37:43 -0700

    Report a brutality incident

--- crisisguard.py
+++ crisisguard.py
@@ -83,3 +83,48 @@ def report_medical_emergency():
     # A wrong option has been selected
     else:
         print("Invalid selection. Please select between 1 and 2. Note that the emergency number is 112".)
+
+""" Script to record brutality incidents """
+
+def gather_info():
+    """Gathers information about the brutality incident from the user"""
+    user_info = {}
+
+    user_info['user_name'] = input("Please write your name and surname: ")
+    user_info['age'] = input("Please write your age: ")
+    user_info['location'] = input("Where is the incident venue? Please give a location or a landmark within 1km if you are in doubt: ")
+
+    # Prompt the user to specify when the incident occurred
+    date_prompt = "Please choose one of the following options:\n"
+    date_prompt += "1. The incident is happening now. I require immediate assistance.\n"
+    date_prompt += "2. The incident is happening now. I do not need immediate assistance.\n"
+    date_prompt += "3. The incident took place in the past."
+    user_info['date'] = input(date_prompt)
+
+    # Prompt the user to specify their role in the incident
+    role_prompt = "Please choose one of the following options:\n"
+    role_prompt += "1. I am the victim\n"
+    role_prompt += "2. I am a witness\n"
+    role_prompt += "3. I am a culprit"
+    user_info['role'] = input(role_prompt)
+
+    # Prompt the user to describe the situation
+    user_info['situation_description'] = input("Please briefly describe the incident. Do not leave out any details. Specify the date or period if the incident took place in the past: ")
+
+    return user_info
+
+# Displays a summary of the gathered informations so the user can verify the infomation
+def display_summary(user_info):
+    """Displays a summary of the gathered information"""
+    print("\nSummary of the information provided:")
+    print(f"Name: {user_info['user_name']}")
+    print(f"Age: {user_info['age']}")
+    print(f"Date: {user_info['date']}")
+    print(f"Location: {user_info['location']}")
+    print(f"Role in the event: {user_info['role']}")
+    print(f"This is what happened: {user_info['situation_description']}")
+
+    if user_info['date'] == '1':
+        print("\nCall one of the following services according to your need:\nPolice: 911\nEmergency: 112\nFire department: 101")
+
+    print("Thank you for reporting this incident. You will receive updates on progress when we start examining your case.")
